6a37457272ad080f5066c377ada6ca5da6af51ff
Thibaud Colas <thibaudcolas@gmail.com>
2015-01-10 13:26:51 +1300

    Start adding rules. No output except to the console

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
-(function () {
+(function (FormValidator) {
   'use strict';
 
-  console.log('Hello!');
-})();
+  var rules = [
+
+    {
+      name: 'email',
+      rules: 'required|valid_email'
+    },
+    {
+      name: 'password',
+      rules: 'required|min_length[9]'
+    },
+    {
+      name: 'colour',
+      display: 'required',
+      rules: 'required'
+    },
+    {
+      name: 'animal',
+      // TODO: Add custom rule for "at least two values".
+      rules: 'required'
+    },
+    {
+      name: 'tiger_type',
+      // TODO: Add custom rule for "if tiger" case.
+      rules: ''
+    }
+  ];
+
+  var validator = new FormValidator('awesome_form', rules, function(errors) {
+    if (errors.length > 0) {
+      console.table(errors);
+    }
+  });
+
+}(window.FormValidator));
