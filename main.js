bf7c16383b93f150ed2c94a5f3046343b6f99f05
Thibaud Colas <thibaudcolas@gmail.com>
2015-01-10 13:37:57 +1300

    Add naive DOM modification to reflect errors

--- main.js
+++ main.js
@@ -31,7 +31,14 @@
   var validator = new FormValidator('awesome_form', rules, function(errors) {
     if (errors.length > 0) {
       console.table(errors);
+      errors.forEach(displayError);
     }
   });
+
+  function displayError(error) {
+    var parent = error.element.parentNode;
+    // TODO: Handle case where error is triggered multiple times on the same element.
+    parent.className = parent.className + ' error';
+  }
 
 }(window.FormValidator));
